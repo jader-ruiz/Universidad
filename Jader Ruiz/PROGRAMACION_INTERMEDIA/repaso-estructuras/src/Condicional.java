0c91257f6c022160caa60daae1c4195fddc2484b
StivenG777-web <guerrerorivasjhonstiven@gmail.com>
2026-09-16 10:58:28 -0500

    Estructura

--- Jader Ruiz/PROGRAMACION_INTERMEDIA/repaso-estructuras/src/Condicional.java
+++ Jader Ruiz/PROGRAMACION_INTERMEDIA/repaso-estructuras/src/Condicional.java
@@ -16,5 +16,36 @@ public class Condicional{
             System.out.println("Mayor de edad");
         }
         String mensaje = (edad >= 18) ? "Mayor de edad" : "Menor de edad"; // If ternario
+
+        int dia = 3;
+        String nombreDia;
+
+        switch (dia) {
+            case 1:
+                nombreDia = "Lunes";
+                break;
+            case 2:
+                nombreDia = "Martes";
+                break;
+            case 3:
+                nombreDia = "Miercoles";
+                break; 
+            case 4:
+                nombreDia = "Jueves";
+                break;
+            case 5:
+                nombreDia = "Viernes";
+                break;
+            case 6:
+                nombreDia = "Sabado";
+                break;
+            case 7:
+                nombreDia = "Domingo";
+                break;
+            default:
+                nombreDia = "Dia no valido";
+                break;
+        }
+        System.out.println("El dia es: "+nombreDia);
     }
 }
